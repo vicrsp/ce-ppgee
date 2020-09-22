5e7fad91fe5a62d3c8d602f7be6aa6ef8c7ca082
Victor Ruela <victorspruela@gmail.com>
2020-09-21 22:00:32 -0300

    - progresso no tp2

--- src/notebooks/main.py
+++ src/notebooks/main.py
@@ -6,5 +6,5 @@ def quad_func(x): return x[0]**2 + x[1]**2
 
 # Executar o algoritmo
 pso_unimodal = PSO(quad_func, [-10, -10], [10, 10], max_feval=1000,
-                   swarm_size=30, acceleration=[1.5, 1.5], constrition=0.5, topology='lbest')
+                   swarm_size=10, acceleration=[1.5, 1.5], constrition=0.5, topology='lbest')
 pso_unimodal.run()
--- src/notebooks/pso.py
+++ src/notebooks/pso.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class PSO:
-    def __init__(self, func, lb, ub, max_feval=10000, swarm_size=100, acceleration=[0.01, 0.01], constrition=1, inertia=0.7, topology='gbest'):
+    def __init__(self, func, lb, ub, max_feval=10000, swarm_size=100, acceleration=[0.01, 0.01], constrition=1, inertia=0.7, topology='gbest', tol=0.001):
         self.max_feval = max_feval
         self.func = func
         self.swarm_size = swarm_size
@@ -14,6 +14,8 @@ class PSO:
         self.x = constrition
         self.w = inertia
         self.c1, self.c2 = acceleration
+        self.tol = tol
+        self.window = 10
 
         self.fevals = 0
         self.global_best_cost = np.Infinity
@@ -39,6 +41,9 @@ class PSO:
         swarm_cost = []
         for index, sol in enumerate(swarm):
             cost = self.func(sol)
+            # # applies barrier function
+            # if(np.any(self.lb - sol > 0) | np.any(self.ub - sol < 0)):
+            #     cost = np.Infinity
             swarm_cost.append(cost)
 
             # Update global best cost
@@ -70,31 +75,13 @@ class PSO:
         elif(self.topology == 'lbest'):
             solution = np.zeros_like(swarm)
             for i in range(self.swarm_size):
-                if(i == 0):
-                    neighbors = np.vstack(
-                        (swarm[-1, :], swarm[0, :], swarm[1, :]))
-                    neighbors_cost = np.array(
-                        [swarm_cost[-1], swarm_cost[0], swarm_cost[1]])
-                    best_neighbor = np.argmin(neighbors_cost)
-                elif(i == (self.swarm_size - 1)):
-                    neighbors = np.vstack(
-                        (swarm[-2, :], swarm[-1, :], swarm[0, :]))
-                    neighbors_cost = np.array(
-                        [swarm_cost[-2], swarm_cost[-1], swarm_cost[0]])
-                    best_neighbor = np.argmin(neighbors_cost)
-                else:
-                    neighbors = np.vstack(
-                        (swarm[i-1, :], swarm[i, :], swarm[i+1, :]))
-                    neighbors_cost = np.array(
-                        [swarm_cost[i-1], swarm_cost[i], swarm_cost[i+1]])
-                    best_neighbor = np.argmin(neighbors_cost)
-
+                neighbors = np.vstack(
+                    (swarm[(i-1) % self.swarm_size, :], swarm[i, :], swarm[(i+1) % self.swarm_size, :]))
+                neighbors_cost = np.array([swarm_cost[(
+                    i-1) % self.swarm_size], swarm_cost[i], swarm_cost[(i+1) % self.swarm_size]])
+                best_neighbor = np.argmin(neighbors_cost)
                 solution[i, :] = neighbors[best_neighbor, :]
 
-                # if(neighbors_cost[best_neighbor] < swarm_cost[i]):
-                #     solution[i, :] = neighbors[best_neighbor]
-                # else:
-                #     solution[i, :] = swarm[i]
             return solution
 
     def update_particles_velocity(self, swarm, swarm_cost):
@@ -104,8 +91,8 @@ class PSO:
         social_best_sol = self.get_social_component_solution(swarm, swarm_cost)
 
         for i, particle in enumerate(swarm):
-            r1 = np.random.random(size=self.num_variables)
-            r2 = np.random.random(size=self.num_variables)
+            r1 = np.random.rand(self.num_variables)
+            r2 = np.random.rand(self.num_variables)
 
             cognitive_component = self.c1 * r1 * \
                 (self.personal_best_solution[i, :] - particle)
@@ -123,29 +110,51 @@ class PSO:
 
         self.swarm_velocity = np.array(updated_velocities)
         self.swarm = np.array(updated_swarm)
+        self.f_tol = np.abs(np.mean(self.swarm_velocity))
 
-    def run(self, debug=True):
+    def validate_boundaries(self, swarm):
+        for index, particle in enumerate(swarm):
+            for i in range(self.num_variables):
+                if(particle[i] - self.lb[i] < 0):
+                    particle[i] = particle[i] - 2 * (particle[i] - self.lb[i])
+                if(particle[i] - self.ub[i] > 0):
+                    particle[i] = particle[i] - 2 * (particle[i] - self.ub[i])
+            self.swarm[index, :] = particle
+
+    def run(self, f_star=None, debug=True):
         self.initialize_swarm()
+        self.f_tol = np.Infinity
+        num_generations = 0
         while(self.fevals < self.max_feval):
             # Calculating the cost of each particle in the swarm.
             self.evaluate_swarm_cost_function(self.swarm)
             self.update_particles_velocity(self.swarm, self.swarm_cost)
+            self.validate_boundaries(self.swarm)
+
+            if(f_star != None):
+                if (np.abs((self.global_best_cost - f_star)/f_star) < self.tol):
+                    break
+
+            if(num_generations > self.window):
+                if (np.mean(np.abs(np.diff(self.iteration_average_cost[-self.window:]))) < self.tol):
+                    break
+
+            num_generations = num_generations + 1
 
         if(debug):
             self.report()
 
-        return self.global_best_solution, self.global_best_cost
+        return self.global_best_solution, self.global_best_cost, self.iteration_average_cost, self.iteration_global_best_cost
 
     def report(self):
-        print('Best global particle: {}'.format(self.global_best_solution))
-        print('Best global cost: {}'.format(self.global_best_cost))
+        print('Best particle: {}'.format(self.global_best_solution))
+        print('Best cost: {}'.format(self.global_best_cost))
         self.plot_charts()
 
     def plot_charts(self):
-        _, ax = plt.subplots(3, 1)
-        ax[0].plot(self.iteration_personal_best_cost)
-        ax[1].plot(self.iteration_average_cost)
-        ax[2].plot(self.iteration_global_best_cost)
-        # plt.legend([''])
+        _, ax = plt.subplots()
+        ax.plot(self.iteration_average_cost, 'b.-')
+        ax.plot(self.iteration_global_best_cost, 'r.-')
+        plt.legend(['Swarm average cost', 'Global best cost'])
         plt.tight_layout()
         plt.show()
